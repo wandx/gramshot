ddc0d273c4daf7ec653fe4c20e9980b266f1d04e
Wandy Purnomo <wandypurnomo92@gmail.com>
2018-12-28 11:35:24 +0700

    cleanup code

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:gramshot/app_model.dart';
 import 'package:gramshot/screens/root.dart';
 
-
-void main(){
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp
-  ]).then((_){
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
     runApp(Root(model: (new AppModel())));
   });
-
 }
